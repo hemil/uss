44f07c507766554a96be0c3084139b5f1fcbbfb8
hemil <hemil.shah@grofers.com>
2017-08-03 17:59:12 +0530

    heroku files

--- enterprise/views/keys_view.py
+++ enterprise/views/keys_view.py
@@ -1,13 +1,11 @@
 from __future__ import unicode_literals
 import logging
-import json
 
-from django.http import HttpResponse
 from django.core.management import call_command
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import ParseError
 
-from modules.utilities import verify_key
+from modules.utilities import verify_key, get_response
 
 logger = logging.getLogger("enterprise")
 
@@ -15,23 +13,14 @@ logger = logging.getLogger("enterprise")
 @api_view(["POST", "PUT", "DELETE"])
 def key_handler(request):
     if request.method == "POST":
+        # For simplicity. On prod, put some auth on this or remove it completely and operate it from your aws machine
         generated_key = call_command('generate_key')
-        return HttpResponse(json.dumps({
-                "status": 1,
-                "message": "New Key Generated",
-                "error_code": 201,
-                "data": generated_key
-            }), content_type="application/json", status=201)
+        return get_response(generated_key, message="New Key Generated", status_code=201)
     if request.method == "PUT":
         api_key = request.META.get("HTTP_API_KEY")
         verify_key(api_key)
         generated_key = call_command('regenerate_key', old_key=api_key)
         if generated_key:
-            return HttpResponse(json.dumps({
-                    "status": 1,
-                    "message": "New Key Generated",
-                    "error_code": 201,
-                    "data": generated_key
-                }), content_type="application/json", status=201)
+            return get_response(generated_key, message="New Key Generated", status_code=201)
         else:
             raise ParseError("Old Key Details missing")
